2ea1a8c1c4d697d4d6602e6f2478c559d8db142f
Girish Ram <girishram.madhusoodanan1@t-mobile.com>
2020-02-13 19:17:00 +0530

    Task1a: Issues fixed

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StocksComponent } from './stocks.component';
+import { FormsModule, ReactiveFormsModule  } from '../../../../../../node_modules/@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule } from '../../../../../../node_modules/@angular/material';
+import { SharedUiChartModule } from '@coding-challenge/shared/ui/chart';
+import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { StoreModule } from '@ngrx/store';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 
 describe('StocksComponent', () => {
   let component: StocksComponent;
@@ -8,7 +14,11 @@ describe('StocksComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StocksComponent ]
+      imports: [FormsModule, ReactiveFormsModule, MatFormFieldModule,
+        MatInputModule, MatSelectModule, MatButtonModule,
+        SharedUiChartModule, StoreModule.forRoot({}), NoopAnimationsModule],
+      declarations: [StocksComponent],
+      providers: [PriceQueryFacade]
     })
     .compileComponents();
   }));
